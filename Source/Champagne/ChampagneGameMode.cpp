cdb2ed8099670c5ad324d1a51b7d558cdb72c571
Sangha Bae <sangha972@gmail.com>
2023-08-24 17:48:42 +0900

    Init Project

--- Source/Champagne/ChampagneGameMode.cpp
+++ Source/Champagne/ChampagneGameMode.cpp
@@ -7,7 +7,7 @@
 AChampagneGameMode::AChampagneGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/AssetPacks/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
